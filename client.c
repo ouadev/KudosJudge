e979cb42f7aa931ab7ffe35110388908f2ff4d8a
ouadev <ouadimjamal@gmail.com>
2017-02-23 03:23:13 +0000

    enhance tester client, by supporting reading from source code and feed from files

--- client.c
+++ client.c
@@ -13,28 +13,38 @@
 #include <sys/time.h>
 
 
-/**
- * struct int_response
- * the structure to be used multiple times during the same connection
- */
-typedef struct int_response{
-	char echo[15]; //< should be filled with the int_request.echo
-	int verdict; //< a number that represents different verdicts
-	char verdict_s[60]; //< the verdict in string format.
-} int_response;
-
+//low-level network function
 int kudosjudge_send(int client_sock, char* s);
+char* kudosjudge_receive(int client_sock);
+
+//business primitives
+char* kudosdjudge_forge_dd(char* source_filename, char* input_filename, char* output_filename);
 void print_hr_duration(unsigned long useconds);
 
-
+/**
+*
+* ./client 
+*/
 int main(int argc , char *argv[])
 {
+	//parse arguments
+
+	if(argc<4){printf("need more args\n");return 0;}
+	char* source_filename=argv[1];
+	char* input_filename=argv[2];
+	char* output_filename=argv[3];
+
+	//TEST
+	struct timeval tv_start, tv_end;
+	unsigned long useconds;
+	int sock;
+	struct sockaddr_in server;
+	int sent;
 	///////
 	//connection///
 	//////////
-	int sock;
-	struct sockaddr_in server;
-	char  server_reply[2000];
+	
+	//char  server_reply[2000];
 
 	//Create socket
 	sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -57,82 +67,27 @@ int main(int argc , char *argv[])
 	}
 
 	puts("KudosClient: Connected\n");
-	/////////////////
-	//// Request/////
-	////////////////
-	////record start time
-	struct timeval tv_start;
+	
+
+	//request
+	char* request=kudosdjudge_forge_dd(source_filename, input_filename,output_filename);
+
+	//record start time
 	gettimeofday(&tv_start,NULL);
-	///build request string.
-	int type=0;
-	int in_type=0;//data
-	int out_type=0;//data
-	char* sourcecode="#include <stdio.h>\nvoid main(){\nwhile(1){};printf(\"Hello KudosJudge\");\n}";
-	char* input		="nothing\nspecial";
-	char* output	="Hello KudosJudge";
-	char* body=(char*)malloc(sizeof(char)*(strlen(sourcecode)+strlen(input)+strlen(output)+400));
-	char* request=(char*)malloc(sizeof(char)*(strlen(sourcecode)+strlen(input)+strlen(output)+400));
-	sprintf(body,"%d\n%d\n%ld\n%s\n%d\n%ld\n%s\n%ld\n%s", 
-		type, in_type,strlen(input), input,out_type, strlen(output), output, strlen(sourcecode), sourcecode );
-	int bodylen=strlen(body);
-	sprintf(request, "%d\n%s",bodylen, body );
-	free(body);
 	//send data
-	//int wrt_count=write(sock, request, strlen(request));
-
-	//send data
-	int sent= kudosjudge_send(sock, request);
-	//receive Judge's response
-	 int_response response;
-	 int rcv_count=0;
-
-
+	sent= kudosjudge_send(sock, request);
 	
-	///////////////
-	 //// receive/////
-	 //////////////////
 	//receive the verdict
-	char* tmp=server_reply;
-	int size_expected=0;
-	int metadata_got=0;
-	int rcv_count_acc=0;
-	while ( 1){
-		rcv_count=recv(sock,tmp, sizeof(server_reply),0 );
-		if(rcv_count<0 && errno!=EAGAIN && errno!=EWOULDBLOCK){
-			puts("KudosClient: recv failed,");
-			printf("KudosClient : error : %s\n", strerror(errno));
-			return 54;
-		}
-		tmp[rcv_count]='\0';
-		if( !metadata_got){
-			char* pp;
-			if( (pp=strchr(tmp, '\n') )!=NULL){
-				metadata_got=1;
-				size_expected=(pp-tmp)+1+atoi(tmp);
-			}
-		}
-		rcv_count_acc+=rcv_count;
-		tmp+=rcv_count;
-		if(rcv_count_acc>=size_expected) break;
-		
+	char* server_reply=kudosjudge_receive(sock);
 
-	} 
-	*(tmp)='\0';
-	int body_len=0;
-	char verdict_s[50];
-	body_len=atoi(server_reply);
-	int i=0;
-	while(server_reply[i]!='\n') i++;i++;
-	strcpy(verdict_s, &server_reply[i]);
-	printf("Verdict : %s\n", verdict_s);
+	printf("Verdict : %s\n", server_reply);
 	
-	/////record end time
-	struct timeval tv_end;
+	//record end time
 	gettimeofday(&tv_end,NULL);
-	unsigned long useconds= (1000000*tv_end.tv_sec + tv_end.tv_usec) - (1000000*tv_start.tv_sec + tv_start.tv_usec);
+	useconds= (1000000*tv_end.tv_sec + tv_end.tv_usec) - (1000000*tv_start.tv_sec + tv_start.tv_usec);
 	print_hr_duration(useconds);
 
-//clean up
+	//clean up
 	close(sock);
 	return 0;
 
@@ -157,10 +112,115 @@ int kudosjudge_send(int client_sock, char* s){
 
 }
 
+char* kudosjudge_receive(int client_sock){
+	char init_buffer[100];
+	int rcv_count=0, rcv_count_acc=0, size_expected=0, metadata_got=0;
+	while(1){
+		rcv_count=recv(client_sock,init_buffer, sizeof(init_buffer),0 );
+		if(rcv_count<0 && errno!=EAGAIN && errno!=EWOULDBLOCK){
+			puts("KudosClient: recv failed,");
+			printf("KudosClient : error : %s\n", strerror(errno));
+			return NULL;
+		}
+		init_buffer[rcv_count]='\0';
+		rcv_count_acc+=rcv_count;
+		char* pp;
+		if( (pp=strchr(init_buffer, '\n') )!=NULL){
+			size_expected=(pp-init_buffer)+1+atoi(init_buffer);
+			break;
+		}
+	}
+	//continue getting
+	char* buffer=(char*)malloc(sizeof(char) * size_expected+10);
+	memcpy(buffer, init_buffer, rcv_count_acc);
+	char* tmp=buffer+rcv_count_acc;
+	while(1){
+		rcv_count=recv(client_sock, tmp, size_expected, 0);
+		if(rcv_count<0 && errno!=EAGAIN && errno!=EWOULDBLOCK){
+			puts("KudosClient: recv failed,");
+			printf("KudosClient : error : %s\n", strerror(errno));
+			return NULL;
+		}
+		rcv_count_acc+=rcv_count;
+		if(rcv_count_acc>= size_expected) break;
+	}
+	*(tmp)='\0';
+	//remove haeder (size of message)
+	int body_len=0;
+	char verdict_s[50];
+	body_len=atoi(buffer);
+	int i=0;
+	while(buffer[i]!='\n') i++;i++;
+	char* final_buffer=(char*)malloc(sizeof(char)*strlen(buffer));
+	strcpy(final_buffer, buffer+i);
+	free(buffer);
+	return final_buffer;
+}
 
 
 void print_hr_duration(unsigned long useconds){
 
 		printf("%ld ms %ld us\n", useconds/1000, useconds%1000);
 	
+}
+
+
+char* kudosdjudge_forge_dd(char* source_filename, char* input_filename, char* output_filename){
+	FILE* source_file=fopen(source_filename,"r");
+	FILE* input_file=fopen(input_filename, "r");
+	FILE* output_file=fopen(output_filename, "r");
+
+	char source[1000];source[0]='\0';
+	char input[4000];input[0]='\0';
+	char output[4000];output[0]='\0';
+
+	char block_buffer[1000];
+
+	 int read=0;
+
+	while(!feof(source_file)){
+		read=fread(block_buffer,sizeof(char), sizeof(block_buffer), source_file);
+		block_buffer[read]='\0';
+		strcat(source, block_buffer);
+	}
+
+	while(!feof(input_file)){
+		read=fread(block_buffer,sizeof(char), sizeof(block_buffer), input_file);
+		block_buffer[read]='\0';
+		strcat(input, block_buffer);	
+	}
+
+
+	while(!feof(output_file)){
+		read=fread(block_buffer,sizeof(char), sizeof(block_buffer), output_file);
+		block_buffer[read]='\0';
+		strcat(output, block_buffer);
+	}
+
+
+	//printf("source:\n%s\n", source);
+	//printf("input:\n%s\n", input );
+	//printf("output:\n%s\n", output );
+	
+	///build request string.
+	int type=0;
+	int in_type=0;//data
+	int out_type=0;//data
+
+	char* body=(char*)malloc(sizeof(char)*(strlen(source)+strlen(input)+strlen(output)+400));
+	char* request=(char*)malloc(sizeof(char)*(strlen(source)+strlen(input)+strlen(output)+400));
+	sprintf(body,"%d\n%d\n%ld\n%s\n%d\n%ld\n%s\n%ld\n%s", 
+		type, in_type,strlen(input), input,out_type, strlen(output), output, strlen(source), source );
+	int bodylen=strlen(body);
+	sprintf(request, "%d\n%s",bodylen, body );
+	free(body);
+
+
+	return request;
+
+
+	
+
+	
+
 }
